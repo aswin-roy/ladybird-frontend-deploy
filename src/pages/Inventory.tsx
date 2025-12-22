37afda9b798dca9c38657a07dd4f5e7bb4015ef7
Aswin <aswinroyrocks@gmail.com>
2025-12-22 19:52:30 +0530

    Update Inventory.tsx

--- src/pages/Inventory.tsx
+++ src/pages/Inventory.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Product } from '../types/types';
 import { InputField, SelectField, Pagination } from '../components';
 import { productService } from '../services/productService';
